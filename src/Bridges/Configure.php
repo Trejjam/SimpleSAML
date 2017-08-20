d6bdbf7629a8c0db91796bece4b5f96870834e7d
Jan Trejbal <jan.trejbal@gmail.com>
2017-08-20 19:03:48 +0200

    Autocreate temp and log directory

--- src/Bridges/Configure.php
+++ src/Bridges/Configure.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jam
- * Date: 31.8.15
- * Time: 15:06
- */
 
 namespace Trejjam\SimpleSAML\Bridges;
 
-
-use Nette,
-	Trejjam;
+use Trejjam;
 
 class Configure
 {
--- src/DI/SimpleSAMLExtension.php
+++ src/DI/SimpleSAMLExtension.php
@@ -1,38 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jan
- * Date: 26. 10. 2014
- * Time: 17:38
- */
 
 namespace Trejjam\SimpleSAML\DI;
 
-use Nette,
-	Trejjam;
+use Trejjam;
 
 class SimpleSAMLExtension extends Trejjam\BaseExtension\DI\BaseExtension
 {
 	protected $default = [
-		'configurationDir' => '%appDir%/config/simpleSAML/config'
+		'configurationDir'  => '%appDir%/config/simpleSAML/config',
+		'configurationFile' => 'config.php',
+		'authsourcesFile'   => 'authsources.php', //unused
 	];
 
 	protected $classesDefinition = [
 		'configure' => 'Trejjam\SimpleSAML\Bridges\Configure',
 	];
 
-	//protected $factoriesDefinition = [
-	//'fooFactory' => 'Trejjam\...',
-	//];
-
-	public function beforeCompile() {
+	public function beforeCompile()
+	{
 		parent::beforeCompile();
 
 		$config = $this->createConfig();
+
 		$classes = $this->getClasses();
-		$classes['configure']->setArguments([
-			$config['configurationDir'],
-		]);
+		$classes['configure']->setArguments(
+			[
+				$config['configurationDir'],
+			]
+		);
 		$classes['configure']->addTag('run');
+
+		$this->createDirectories($config);
+	}
+
+	private function createDirectories(array $extensionConfig)
+	{
+		$config = [];
+		include($extensionConfig['configurationDir'] . DIRECTORY_SEPARATOR . $extensionConfig['configurationFile']);
+
+		if (array_key_exists('loggingdir', $config)) {
+			@mkdir($config['loggingdir'], 0775, TRUE);
+		}
+		if (array_key_exists('tempdir', $config)) {
+			@mkdir($config['tempdir'], 0775, TRUE);
+		}
 	}
 }
